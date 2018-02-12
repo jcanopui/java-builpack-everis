082d7c47479aec0e5823e9932ba550593272a99d
Marc Soldevilla Cuartiella <msoldevi@everis.com>
2018-02-12 17:44:12 +0100

    fixed expect

--- spec/java_buildpack/jre/open_jdk_like_memory_calculator_spec.rb
+++ spec/java_buildpack/jre/open_jdk_like_memory_calculator_spec.rb
@@ -84,8 +84,7 @@ describe JavaBuildpack::Jre::OpenJDKLikeMemoryCalculator do
 
     command = component.memory_calculation_command
 
-    expect(command).to eq('echo JVM Memory ' \
-                            'Configuration: && JAVA_OPTS="$JAVA_OPTS "')
+    expect(command).to eq('JAVA_OPTS="$JAVA_OPTS "')
   end
 
   it 'does not throw an error when a directory ends in .jar',
